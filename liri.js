b6e17c7e8816d66dd8fce7eeece9c8a2a79f83a6
divya <gopalakrishnandivya6@gmail.com>
2018-12-28 14:35:46 -0700

    commit6

--- liri.js
+++ liri.js
@@ -10,43 +10,58 @@ var axios = require("axios");
 // `node liri.js movie-this '<movie name here>'`
 // Store all of the arguments in an array
 
-// empty variable to hold movie name
-var movieName = "";
-// var action=process.argv[2];
 
-// Loop through all the words in the node argument
-for (var i = 2; i <process.argv.length; i++) {
+var action = process.argv[2];
+switch (action) {
+    case "movie-this":
+        moviethis();
+        break;
 
-  if (i > 2 && i < process.argv.length) {
-    movieName = movieName + "+" + process.argv[i];
-  }
-  else {
-    movieName += process.argv[i];
-
-  }
-
+    case "movie-this":
+        nomoviein();
+        break;
 }
-console.log(movieName)
+function moviethis() {
+    // empty variable to hold movie name
+    var movieName = "";
+    // Loop through all the words in the node argument
+    for (var i = 3; i < process.argv.length; i++) {
 
-// the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-console.log(queryUrl);
+        if (i > 3 && i < process.argv.length) {
+            movieName = movieName + "+" + process.argv[i];
+        }
+        else {
+            movieName += process.argv[i];
 
-axios.get(queryUrl).then(
-  function(response) {
-    // console.log(response.data);
-    console.log(JSON.stringify(response.data, null, 2));
+        }
 
-    console.log('\nTitle of the movie is '+JSON.stringify(response.data.Title)+'\nYear the movie came out-  '+ JSON.stringify(response.data.Year) 
-    +'\nIMDB Rating of the movie- '+ JSON.stringify(response.data.imdbRating)+'\nRotten Tomatoes Rating of the movie- '
-    +JSON.stringify(response.data.Ratings[1])+'\nCountry where the movie was produced- '+JSON.stringify(response.data.Country)+
-    '\nLanguage of the movie- '+JSON.stringify(response.data.Language)+'\nPlot of the movie- '+JSON.stringify(response.data.Plot)+'\nActors in the movie- ' +JSON.stringify(response.data.Actors) );
+    }
+    console.log(movieName);
 
-    
-    
+    // the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function (response) {
+            // console.log(response.data);
+            console.log(JSON.stringify(response.data, null, 2));
+            // console the needed informations
+            console.log('\nTitle of the movie is ' + JSON.stringify(response.data.Title) + '\nYear the movie came out-  ' + JSON.stringify(response.data.Year)
+                + '\nIMDB Rating of the movie- ' + JSON.stringify(response.data.imdbRating) + '\nRotten Tomatoes Rating of the movie- '
+                + JSON.stringify(response.data.Ratings[1]) + '\nCountry where the movie was produced- ' + JSON.stringify(response.data.Country) +
+                '\nLanguage of the movie- ' + JSON.stringify(response.data.Language) + '\nPlot of the movie- ' + JSON.stringify(response.data.Plot) + '\nActors in the movie- ' + JSON.stringify(response.data.Actors));
+
+
+
+        }
+    );
 }
-);
 
-
+function nomoviein() {
+    
+    console.log("Mr.Nobody");
+    console.log("If you haven't watched Mr. Nobody, then you should: " + " http://www.imdb.com/title/tt0485947/" + " It's on Netflix!")
+}
 
 
